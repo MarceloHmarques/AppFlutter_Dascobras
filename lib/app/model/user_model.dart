ec48ad96165e51b796f7493033babf37976558fb
DaviFreita <davi.freitas@alunos.ufersa.edu.br>
2026-05-30 11:58:19 -0300

    FEAT: adding email for the user

--- lib/app/model/user_model.dart
+++ lib/app/model/user_model.dart
@@ -1,5 +1,6 @@
 class UserModel {
   final String id;
+  String _email;
   String name;
   final String _cpf;
   String _password;
@@ -11,12 +12,16 @@ class UserModel {
     required this.date,
     required String cpf,
     required String password,
+    required String email,
   }) : _cpf = cpf,
-       _password = password;
+       _password = password,
+       _email = email;
 
   String get cpf => _cpf;
 
   String get password => _password;
+
+  String get email => _email;
 
   factory UserModel.fromJson(Map<String, dynamic> json) {
     return UserModel(
@@ -25,6 +30,7 @@ class UserModel {
       cpf: json['cpf'],
       password: json['password'],
       date: json['date'],
+      email: json['email'],
     );
   }
 
@@ -34,5 +40,6 @@ class UserModel {
     'cpf': cpf,
     'password': password,
     'date': date,
+    'email': email,
   };
 }
