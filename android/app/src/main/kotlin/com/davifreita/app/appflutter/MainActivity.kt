d198a8f038648fc4e3265c5483292ab058f4d2fc
DaviFreita <davi.freitas@alunos.ufersa.edu.br>
2026-06-08 21:54:20 -0300

    FIX: Changing the app name and icon

--- android/app/src/main/kotlin/com/davifreita/app/appflutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/davifreita/app/appflutter/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.davifreita.app.appflutter
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterFragmentActivity()
